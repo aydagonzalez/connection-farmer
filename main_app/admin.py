4dc96bd9fe4197f7e5caf93543e31730fd46a594
LY <eayetc@gmail.com>
2024-04-09 09:36:22 -0700

    added event model and detail pages

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Job
+from .models import Profile, Job, Event
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Job)
+admin.site.register(Event)
---
+++ main_app/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from .models import Job
+
+class JobForm(ModelForm):
+  class Meta:
+    model = Job
+    fields = '__all__'
+    
+    # START HERE
+
